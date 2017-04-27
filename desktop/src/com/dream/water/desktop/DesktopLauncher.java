111ce50987f675d45a2d21916c4bfd512c90b0b7
José Carlos Solís Lojo <jcsolis@atsistemas.com>
2017-04-27 09:04:55 +0200

    Basic structure

---
+++ desktop/src/com/dream/water/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dream.water.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dream.water.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
